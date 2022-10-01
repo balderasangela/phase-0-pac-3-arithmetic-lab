0c668522e3370fbabb4b37e0b810e731f1b5045c
Angela <balderasangela@gmail.com>
2022-09-30 20:52:46 -0400

    COMPLETED ASSIGNMENT

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+let number = 0
 
 
 function add(a, b) {
@@ -19,14 +19,12 @@ function divide(a, b){ //divide a and b and return the result.
   return number;
 }
 
-let number = 0
 
-function Increment(n){ //Increment a by 1 and return the result.
-  return (number += 1);
+function increment(a, b){ //Increment a by 1 and return the result.
+  return (number = ++a);
 }
-function Decrement(n){ //Decrement a by 1 and return the result.
-  number = a - 1;
-  return number;
+function decrement(a, b){ //Decrement a by 1 and return the result.
+  return (number = --a);
 }
 function makeInt(a){ //Parse a as an integer and return that integer.
   number = parseInt(a, 10);
