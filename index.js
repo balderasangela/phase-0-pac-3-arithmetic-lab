8e713bbdd722e3dfa34130639dd824e8e3dfa2cc
Angela <balderasangela@gmail.com>
2022-09-30 20:30:50 -0400

    completed assignment

---
+++ index.js
@@ -0,0 +1,40 @@
+
+
+
+function add(a, b) {
+    number = a + b;
+    return number
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    number = a * b;
+  return number;
+}
+function divide(a, b){ //divide a and b and return the result.
+  number = a / b;
+  return number;
+}
+
+let number = 0
+
+function Increment(n){ //Increment a by 1 and return the result.
+  return (number += 1);
+}
+function Decrement(n){ //Decrement a by 1 and return the result.
+  number = a - 1;
+  return number;
+}
+function makeInt(a){ //Parse a as an integer and return that integer.
+  number = parseInt(a, 10);
+  return number;
+}
+function preserveDecimal(a){ //Parse a as a float (number with decimals) and return that number.
+  number = parseFloat(a, 10);
+  return number;
+}
+
+Increment();
